6cbbc9468c39c7b8751eb732c6e25f4a3b482427
Tengizovich <113883231+Tengizovich@users.noreply.github.com>
2023-02-18 13:58:58 +0300

    Create Relation.java

---
+++ Relation.java
@@ -0,0 +1,18 @@
+public enum Relation {
+    FATHER,
+    MOTHER,
+    SON,
+    DAUGHTER,
+    BROTHER,
+    SISTER,
+    UNCLE,
+    AUNT,
+    GRANDMA,
+    GRANDPA,
+    NEPHEW,
+    NIECE,
+    HUSBAND,
+    WIFE,
+    GRANDSON,
+    GRANDDAUGHTER
+}
